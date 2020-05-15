3d7397f049c8d91ff8539037c2be7a05d8f191b6
Lindsey Guan <lindseyguan7@berkeley.edu>
2020-05-14 18:35:31 -0700

    rename file

--- graph_vis.py
+++
@@ -1,8 +0,0 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-
-G1 = nx.read_gml("tf_operon_graph_all.gml")
-plt.subplot(121)
-nx.draw(G1, with_labels=True, font_weight='bold')
-plt.subplot(122)
-nx.draw_shell(G1, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
